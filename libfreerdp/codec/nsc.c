a63afe0f161764dd1af3b211e61bc135c0a3291f
Armin Novak <armin.novak@thincast.com>
2026-02-15 19:32:38 +0100

    [codec,nsc] bounds checks and doxygen
    * Improve doxygen for nsc_process_message
    * Improve bounds checks for nsc_process_message
    
    (cherry picked from commit f4d74c33fd58e9e9e4e52d75b0d1255af8fa4b53)

--- libfreerdp/codec/nsc.c
+++ libfreerdp/codec/nsc.c
@@ -451,13 +451,20 @@ BOOL nsc_process_message(NSC_CONTEXT* context, UINT16 bpp, UINT32 width, UINT32 
 	if (!context || !data || !pDstData)
 		return FALSE;
 
-	s = Stream_New((BYTE*)data, length);
+	if (nXDst > nWidth)
+		return FALSE;
+	if (nYDst > nHeight)
+		return FALSE;
 
+	s = Stream_New((BYTE*)data, length);
 	if (!s)
 		return FALSE;
 
+	const UINT32 minStride = nWidth * GetBytesPerPixel(DstFormat);
 	if (nDstStride == 0)
-		nDstStride = nWidth * GetBytesPerPixel(DstFormat);
+		nDstStride = minStride;
+	if (nDstStride < minStride)
+		return FALSE;
 
 	switch (bpp)
 	{
